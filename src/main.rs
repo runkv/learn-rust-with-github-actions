def2adfdf9c4d2dfe0017fe6cf72352dd644d661
Rune Kvisten <rune.kvisten@gmail.com>
2024-03-23 20:43:03 +0100

    Fixed bug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world! Willthis be working without ;?");
-
+}
 
 // Next, try a more interesting example from the interactive Terminal: https://github.com/glitteringkatie/spongemock
 
