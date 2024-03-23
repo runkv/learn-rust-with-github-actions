772925468ff42df2ecb7922bd2c295a1006f72ec
Rune Kvisten <rune.kvisten@gmail.com>
2024-03-23 20:36:40 +0100

    Bug

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Hello, world! Willthis be working?");
+    println!("Hello, world! Willthis be working?")
 }
 
 // Next, try a more interesting example from the interactive Terminal: https://github.com/glitteringkatie/spongemock
